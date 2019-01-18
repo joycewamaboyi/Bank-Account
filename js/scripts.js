5fea4c3476b24c02e0af6b3e5d87e8549c5ec815
tuyjoy92@yahoo.fr <tuyjoy92@yahoo.fr>
2019-01-18 15:02:58 +0200

    javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -17,24 +17,50 @@ $(function(){
    newAccount.output();
  });
  $("#form-balance").submit(function(event){
-    event.preventDefault();
-    if(currentAccount != "")
-    {
-      var deposit = parseFloat($("#input-deposit").val());
-      var withdraw = parseFloat($("#input-withdraw").val());
-      if(isNaN(deposit))
-      {
-        deposit = 0;
-      }
-      if(isNaN(withdraw))
-      {
-        withdraw = 0;
-      }
-      var temp = deposit-withdraw;
-      //console.log(formatUSD(temp));
-      currentAccount.changeBalance(temp);
-      currentAccount.output();
-    }
-  });
- 
+   event.preventDefault();
+   if(currentAccount != "")
+   {
+     var deposit = parseFloat($("#input-deposit").val());
+     var withdraw = parseFloat($("#input-withdraw").val());
+     if(isNaN(deposit))
+     {
+       deposit = 0;
+     }
+     if(isNaN(withdraw))
+     {
+       withdraw = 0;
+     }
+     var temp = deposit-withdraw;
+     //console.log(formatUSD(temp));
+     currentAccount.changeBalance(temp);
+     currentAccount.output();
+   }
  });
+
+});
+//Create a constructor for object Task
+function BankAccount(inputUserName,inputBalance){
+    this.name = inputUserName;
+    this.balance = inputBalance;
+   }
+   
+   BankAccount.prototype.output = function ()
+   {
+    $(".output-balance").text(formatUSD(this.balance));
+   };
+   
+   BankAccount.prototype.changeBalance = function (amount)
+   {
+    this.balance += amount;
+   }
+   
+   function clear(temp)
+   {
+    $(temp).val("");
+   }
+   
+   function formatUSD(tempString)
+   {
+    return tempString.toLocaleString('en-US',{style: 'currency', currency: 'USD'});
+   }
+   
